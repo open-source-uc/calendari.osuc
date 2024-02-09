b19b1f79f0d0fae16669b56a3c43ab493da52e1e
Benjamín Vicente <benjavicente@uc.cl>
2024-02-09 20:16:48 -0300

    add edge runtime

--- src/app/auth/logout/route.ts
+++ src/app/auth/logout/route.ts
@@ -2,6 +2,8 @@ import { logout } from "@/auth/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+export const runtime = "edge";
+
 export async function GET() {
   await logout();
   revalidatePath("/", "layout");
---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+export const runtime = "edge";
+
+export default function NotFound() {
+  return <h1>No se ha encontrado la página</h1>;
+}
